7c069614d3ecb1c099282835857a63fe1c7d1877
Anthony D. Bryant <antmandb93@live.com>
2017-12-07 11:25:46 -0800

    Added something else

--- Assets/_Scripts/WarGames.cs
+++ Assets/_Scripts/WarGames.cs
@@ -8,6 +8,7 @@ public class WarGames : MonoBehaviour {
 	void Start () {
 		print ("Shall we play a GAME?");
 		print ("Yes, let's play Global Thermonuclear War.");
+		print ("How about a nice game of Chess?");
 	}
 	
 	// Update is called once per frame
